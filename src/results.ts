31775077d9792a514ab9a202ae0176b5f9658ce0
Vizdun <viz@vizarch.localdomain>
2021-09-22 20:50:22 +0200

    back button on results redirects to index url

--- src/results.ts
+++ src/results.ts
@@ -4,6 +4,6 @@ import { resultsHtml } from "./pages/results"
 export function results() {
 	document.body.innerHTML = resultsHtml()
 	document.getElementById("backButton").addEventListener("click", () => {
-		changePage(Page.index)
+		location.search = ""
 	})
 }
