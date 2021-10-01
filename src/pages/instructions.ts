a4a7bab7ef95778a5f439339519de1a8a60ec7e6
Quark <72112478+QUARK88@users.noreply.github.com>
2021-10-01 18:05:28 -0400

    Add files via upload

--- src/pages/instructions.ts
+++ src/pages/instructions.ts
@@ -4,12 +4,11 @@ export const instructions = `<h1>${general.title}</h1>
 <hr />
 <h2 style="text-align: center">Instructions</h2>
 <p class="question">
-	You will be presented with a series of statements. For each one, click the
-	button with your opinion on it.
+	You will see 120 statements. For each, select your degree of agreement.
 </p>
-<button class="button" id="quizButton">Got it!</button>
+<button class="button" id="quizButton">Alright</button>
 <br />
 <button class="button disagree" id="backButton">
-	Wait, nevermind!
+	Nevermind
 </button>
 <br />`
