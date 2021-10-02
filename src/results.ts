0557ae125bf68754730f2b55901808d5cb02a5b7
Quark <72112478+QUARK88@users.noreply.github.com>
2021-10-02 06:42:18 -0400

    Delete src directory

--- src/results.ts
+++
@@ -1,9 +0,0 @@
-import { changePage, Page } from "."
-import { resultsHtml } from "./pages/results"
-
-export function results() {
-	document.body.innerHTML = resultsHtml()
-	document.getElementById("backButton").addEventListener("click", () => {
-		location.search = ""
-	})
-}
