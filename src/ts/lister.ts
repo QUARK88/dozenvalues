7ab681e8575f7b0cfd68f3c4a57e4c0330ad0328
TheGhostOfInky <theghostofinky@gmail.com>
2022-08-30 13:49:05 +0100

    Added absolute toggle

--- src/ts/lister.ts
+++ src/ts/lister.ts
@@ -10,16 +10,35 @@ const scores: Array<number> = rawScores.split(",").map(v => parseFloat(v))
 const ideologies: Array<Ideology> = getJson("ideologies-" + lang)
 const weights: Array<number> = [1, 0.9, 1, 0.6, 0.6, 0.7]
 const matches: Array<Ideology> = orderScores(scores, weights, ideologies)
-const maxValue = Math.max(...matches.map(x => x.score!))
-console.log(maxValue)
-const weighedMatches = matches.map(x => {
-    const weighedScore = x.score!/maxValue
-    x.score =  100*(1-weighedScore)
-    return x
+
+const abscheck = <HTMLInputElement> document.getElementById("abs")
+abscheck.addEventListener("click", () => {
+
+    displayMatches(matches,abscheck.checked)
 })
-const matchholder = <HTMLDivElement> document.getElementById("matchholder")!
-for(const m of weighedMatches) {
-    const elm = document.createElement("p")
-    elm.textContent = `${m.name} : ${m.score?.toFixed(1)}%`
-    matchholder.appendChild(elm)
+
+displayMatches(matches,abscheck.checked)
+
+function displayMatches(matches: Ideology[], absolute: boolean=false) {
+    const maximum = scores.map(x => Math.max(x,100-x))
+    let max = 0
+    for(let i=0;i<maximum.length;i++){
+        max += weights[i] * Math.pow(maximum[i] / 100, 3)
+    }
+    max /= maximum.length
+    const maxValue = Math.max(...matches.map(x => x.score!))
+    const weighedMatches = matches.map(x => {
+        const weighedScore = x.score!/(absolute? maxValue : max)
+        const obj = {} as Ideology
+        obj.score =  100*(1-weighedScore)
+        obj.name = x.name
+        return obj
+    })
+    const matchholder = <HTMLDivElement> document.getElementById("matchholder")!
+    matchholder.innerHTML = ""
+    weighedMatches.forEach( (v,i) => {
+        const elm = document.createElement("p")
+        elm.textContent = `${i+1}: ${v.name} : ${v.score?.toFixed(1)}%`
+        matchholder.appendChild(elm)
+    })
 }
